00fa6fc3ab859432c152cd04408976626bce5eff
PhucSE161448 <lamminhphuc285@gmail.com>
2023-10-02 18:38:03 +0700

    Add View And Create Villa

--- Villa_Web/Controllers/VillaController.cs
+++ Villa_Web/Controllers/VillaController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using Villa_Web.Models;
 using Villa_Web.Models.DTO;
 using Villa_Web.Services.IServices;
@@ -16,7 +17,7 @@ namespace Villa_Web.Controllers
             _villaService = villaService;
             _mapper = mapper;
         }
-
+      
         public async Task<IActionResult> IndexVilla()
         {
             List<VillaDTO> list = new List<VillaDTO>();
@@ -26,6 +27,24 @@ namespace Villa_Web.Controllers
                 list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
         }
     }
 }
--- Villa_Web/Models/DTO/VillaCreateDTO .cs
+++ Villa_Web/Models/DTO/VillaCreateDTO .cs
@@ -9,10 +9,10 @@ namespace Villa_Web.Models.DTO
         public string Name { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
-        public string Details { get; set; }
+        public string? Details { get; set; }
         [Required]
         public double Rate { get; set; }
-        public string ImageUrl { get; set; }
-        public string Amenity { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Amenity { get; set; }
     }
 }
